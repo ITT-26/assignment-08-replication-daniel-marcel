7a3bdb484f3e5f324c8202250e26120d9ee77fa1
Marcel Mozet <marcel.mozet@stud.uni-regensburg.de>
2026-07-06 15:09:49 +0200

    add rotation

--- Blender_extension/blender_dippid.py
+++ Blender_extension/blender_dippid.py
@@ -1,4 +1,5 @@
 import mathutils  # blender math utils
+import math
 import json
 import socket
 import bpy  # blender python api
@@ -23,22 +24,25 @@ class DIPPID_Controller(bpy.types.Operator):
 
     # Tuning variables
     UDP_PORT = 5700
+    DEADZONE = 0.05
+    SENSITIVITY = 5 # Rotation speed multiplier
 
-    SENSITIVITY = 0.2 # Rotation speed multiplier
-    
     acc_x = 0
     acc_y = 0
     acc_z = 0
-    
+
     grav_x = 0
     grav_y = 0
     grav_z = 0
-    
+
     gyro_x = 0
     gyro_y = 0
     grav_z = 0
-    
-    
+
+    pitch = 0
+    roll = 0
+    yaw = 0
+
     def modal(self, context, event):
         # Stop the script if the user presses ESC
         if event.type in {'ESC'}:
@@ -48,6 +52,7 @@ class DIPPID_Controller(bpy.types.Operator):
 
         # Triggered every time the timer fires
         if event.type == 'TIMER':
+            dt = 0.016
             try:
                 # Receive data non-blockingly
                 data, addr = self._sock.recvfrom(1024)
@@ -58,21 +63,30 @@ class DIPPID_Controller(bpy.types.Operator):
                     self.acc_x = acc.get("x", 0)
                     self.acc_y = acc.get("y", 0)
                     self.acc_z = acc.get("z", 0)
-                    
+
                 if "gravity" in payload:
                     grav = payload["gravity"]
                     self.grav_x = grav.get("x", 0)
                     self.grav_y = grav.get("y", 0)
                     self.grav_z = grav.get("z", 0)
-                    
+
                 if "gyroscope" in payload:
                     gyro = payload["gyroscope"]
                     self.gyro_x = gyro.get("x", 0)
                     self.gyro_y = gyro.get("y", 0)
                     self.gyro_z = gyro.get("z", 0)
 
-                self.report({'INFO'}, f"Sensors: {self.acc_z}, {self.grav_x}, {self.gyro_x}")
-                self.rotate_selected_objects(context, self.gyro_x, self.gyro_y, self.gyro_z)
+                # self.report({'INFO'}, f"Sensors: {self.acc_z}, {self.grav_x}, {self.gyro_x}")
+                self.pitch = math.atan2(self.grav_x, math.sqrt(
+                    self.grav_y**2 + self.grav_z ** 2))
+                
+                self.roll = math.atan2(self.grav_y, math.sqrt(
+                    self.grav_y**2 + self.grav_z ** 2))
+                
+                self.yaw += self.gyro_z * dt
+
+                self.rotate_selected_objects(
+                    context, self.pitch, self.roll, self.yaw)
 
             except BlockingIOError:
                 # No data arrived yet, just pass through smoothly
@@ -83,7 +97,7 @@ class DIPPID_Controller(bpy.types.Operator):
         # Let normal Blender events (like regular mouse clicks) pass through
         return {'PASS_THROUGH'}
 
-    def rotate_selected_objects(self, context, x=0, y=0, z=0):
+    def rotate_selected_objects(self, context, pitch=0, roll=0, yaw=0):
         # Ensure we are in a 3D viewport
         if context.area.type != 'VIEW_3D':
             return
@@ -95,10 +109,18 @@ class DIPPID_Controller(bpy.types.Operator):
         # self.report({'INFO'}, f"Selected Objects: {context.selected_objects}")
 
         if context.selected_objects:
+            if abs(pitch) < self.DEADZONE:
+                pitch = 0
+            if abs(roll) < self.DEADZONE:
+                roll = 0
+            if abs(yaw) < self.DEADZONE or pitch > 0 and roll > 0:
+                yaw = 0
             for obj in context.selected_objects:
-                obj.rotation_euler.x += x * self.SENSITIVITY
-                obj.rotation_euler.y += y * self.SENSITIVITY
-                obj.rotation_euler.z += z * self.SENSITIVITY
+                obj.rotation_mode = 'QUATERNION'
+                euler_rotation = mathutils.Euler((roll * self.SENSITIVITY, pitch * self.SENSITIVITY, yaw), 'XYZ')
+                quat_rotation = euler_rotation.to_quaternion()
+                obj.rotation_quaternion = quat_rotation
+                # rv3d.view_rotation  = quat_rotation
 
     def invoke(self, context, event):
         # 1. Setup a NON-BLOCKING UDP Socket
